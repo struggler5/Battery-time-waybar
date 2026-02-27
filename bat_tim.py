97a02a6c0fa1ada3adbb6ee4d32220d0f329a718
Vleju Dumitru <112560005+struggler5@users.noreply.github.com>
2026-02-27 15:21:57 +0200

    fixed formatting

--- bat_tim.py
+++ bat_tim.py
@@ -2,7 +2,15 @@
 
 import os 
 tm =  str(os.popen("upower -i /org/freedesktop/UPower/devices/battery_BAT0").read()).split()
-str = tm[65] + " h"
+
+num = float(tm[65])
+
+if num > 6:
+    form = " m"
+else:
+    form = " h"
+
+str = str(num) + form
 
 if __name__ == "__main__":
     output = {
@@ -10,3 +18,8 @@ if __name__ == "__main__":
         "class":"batt_time"
             }
     print(str)
+
+
+
+
+
